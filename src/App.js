7150fec9c961647a84a0bf0e5de835df68d11286
unknown <3220104364@zju.edu.cn>
2023-08-04 15:27:21 +0800

    remove react first try

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>我不想学大数据可视化了！！！</h1>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a href="https://www.4399.com/" target="_blank">Try your game!</a><br></br>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
