8b224ffec379a45a45325d0807de49f08ab45b13
Star0228 <3220104364@zju.edu.cn>
2023-08-04 18:43:41 +0800

    1

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Getting Started with Create React App
 本仓库是小组共享工作的代码托管区
-第二次尝试111222
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 ## Available Scripts
 
--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
         <img src={logo} className="App-logo" alt="logo" />
         <h1>我不想学大数据可视化了！！！</h1>
         <p>
