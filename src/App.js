df7e4284ba41dbf7fe64a7432963d9eb39374b95
Star0228 <3220104364@zju.edu.cn>
2023-08-05 22:52:09 +0800

    material studying 1

--- src/App.js
+++ src/App.js
@@ -1,27 +1,48 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+
 import './App.css';
-
-function App() {
-  return (
+import SearchAppBar from './components/AppBar';
+import Card from './components/Card';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+  function App() {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>我不想学大数据可视化了！！！</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://www.4399.com/" target="_blank">Try your game!</a><br></br>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Container>
+    <SearchAppBar />
+      <Grid container spacing={5}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </Grid>
+    </Container>
     </div>
-  );
-}
+    );
+  }
+  export default App;
+// function App() {
+// return <div className="App"></div>
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <h1>我不想学大数据可视化了！！！</h1>
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a href="https://www.4399.com/" target="_blank">Try your game!</a><br></br>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+//  }
 
-export default App;
+//  export default App;
